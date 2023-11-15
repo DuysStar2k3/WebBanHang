8e19a14d9e1cd4d14f7c5392a0810ba3786472c6
TeeUsagii <tiennguyen2003.b@gmail.com>
2023-11-15 23:47:09 +0700

    admin quanlybviet

--- Project_Web/admin/modules/quanlybviet/lietke.php
+++ Project_Web/admin/modules/quanlybviet/lietke.php
@@ -1,50 +1,38 @@
 <?php
-$sql_lietke_sp = "SELECT *FROM tbl_sanpham,tbl_danhmuc where tbl_sanpham.id_danhmuc=tbl_danhmuc.id_danhmuc ORDER BY id_sp DESC";
-$query_lietke_sp = mysqli_query($mysqli, $sql_lietke_sp);
+
 ?>
 <h3 class="font-size-24 mg-5">Danh sách vừa nhập</h3>
 <div class="bg-main-item pd-5 bd-5">
 
     <table border="1px" style="width: 100%;">
         <tr>
-            <th>id</th>
-            <th> Tên Sản phẩm</th>
-            <th>Hình ảnh</th>
-            <th>Giá sản phẩm</th>
-            <th>Số lượng</th>
-            <th>Danh mục</th>
-            <th>Mã sản phẩm</th>
-            <th>Tóm tắt</th>
-            <th>Trạng thái</th>
+            <th>ID</th>
+            <th>Tiêu đề</th>
+            <th>Nội dung</th>
+            <th>Ảnh</th>
             <th>Quản lý</th>
         </tr>
         <?php
         $i = 0;
-        while ($row = mysqli_fetch_array($query_lietke_sp)) {
+        while ($row = mysqli_fetch_array($query_lietke_bv)) {
             $i++;
-        ?>
+            ?>
             <tr>
-                <td><?php echo $i ?></td>
-                <td><?php echo $row["tensp"] ?></td>
-                <td><img style="width: 200px;" src="modules/quanlysp/uploads/<?php echo $row['hinhanh']?>" alt=""></td>
-                <td><?php echo $row["giasp"] ?></td>
-                <td><?php echo $row["soluong"] ?></td>
-                <td><?php echo $row["tendanhmuc"] ?></td>
-                <td><?php echo $row["masp"] ?></td>
-                <td><?php echo $row["tomtat"] ?></td>
                 <td>
-                    <?php if ($row["tinhtrang"] == 1) {
-                        echo 'Kích Hoạt';
-                    } else {
-                        echo 'Ẩn';
-                    } ?>
+                    <?php echo $i ?>
                 </td>
-    
                 <td>
-                <td><a href="modules/quanlysp/xuly.php?idsanpham=<?php echo $row['id_sp'] ?>">Xóa</a> 
-                |<a href="?action=quanlysp&query=sua&idsanpham=<?php echo $row['id_sp'] ?>">Sửa</a></td>
+                    <?php echo $row["tenbv"] ?>
+                </td>
+                <td>
+                    <?php echo $row["noidungbv"] ?>
+                </td>
+                <td><img style="width: 200px;" src="modules/quanlybv/uploads/<?php echo $row['hinhanh'] ?>" alt=""></td>
+
+                <td><a href="modules/quanlysp/xuly.php?idsanpham=<?php echo $row['id_sp'] ?>">Xóa</a>
+                    |<a href="?action=quanlysp&query=sua&idsanpham=<?php echo $row['id_sp'] ?>">Sửa</a></td>
             </tr>
-        <?php
+            <?php
         }
         ?>
     </table>
--- Project_Web/admin/modules/quanlybviet/them.php
+++ Project_Web/admin/modules/quanlybviet/them.php
@@ -2,30 +2,40 @@
 <div class="bg-main-item pd-5 bd-5">
 
     <table border="1px" width="50%">
-        <form method="post" action="modules/quanlybviet/xuly.php" enctype="multipart/form-data">
-            <label for="title">Tiêu đề:</label>
-            <input type="text" id="title" name="title" required>
-
-            <label for="content">Nội dung:</label>
-            <textarea id="content" name="content" required></textarea>
-            <label for="image">Danh mục:</label>
-            <select name="danhmuc">
-                <?php
-                $sql_danhmuc = "SELECT *FROM tbl_danhmuc ORDER BY id_danhmuc desc";
-                $query_danhmuc = mysqli_query($mysqli, $sql_danhmuc);
-                while ($row_danhmuc = mysqli_fetch_array($query_danhmuc)) {
-                    ?>
-                    <option value="<?php echo $row_danhmuc['id_danhmuc'] ?>">
-                        <?php echo $row_danhmuc['tendanhmuc'] ?>
-                    </option>
-                    <?php
-                }
-                ?>
-            </select><br>
-            <label for="image">Ảnh:</label>
-            <input type="file" id="image" name="image">
-
-            <button type="button">Thêm Bài Viết</button>
-        </form>
+        <div>
+            <form method="post" action="modules/quanlybviet/xuly.php" enctype="multipart/form-data">
+                <div class="mg-5">
+                    <label for="title">Tiêu đề:</label>
+                    <input type="text" id="title" name="title" required>
+                </div>
+                <div class="mg-5">
+                    <label for="content">Nội dung:</label>
+                    <textarea class="no-resize" rows="20" id="content" name="content" required></textarea>
+                </div>
+                <div class="mg-5">
+                    <label for="image">Danh mục:</label>
+                    <select name="danhmuc">
+                        <?php
+                        $sql_danhmuc = "SELECT *FROM tbl_danhmuc ORDER BY id_danhmuc desc";
+                        $query_danhmuc = mysqli_query($mysqli, $sql_danhmuc);
+                        while ($row_danhmuc = mysqli_fetch_array($query_danhmuc)) {
+                            ?>
+                            <option value="<?php echo $row_danhmuc['id_danhmuc'] ?>">
+                                <?php echo $row_danhmuc['tendanhmuc'] ?>
+                            </option>
+                            <?php
+                        }
+                        ?>
+                    </select><br>
+                </div>
+                <div class="mg-5">
+                    <label for="image">Hình ảnh:</label>
+                    <input type="file" id="image" name="image">
+                </div>
+                <div class="mg-5">
+                    <input type="submit" name="thembv" value="Thêm bài viết">
+                </div>
+            </form>
+        </div>
     </table>
 </div>
