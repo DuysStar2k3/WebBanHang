29d69c24b6902ac20dd81ab155f73b4400349101
TeeUsagii <tiennguyen2003.b@gmail.com>
2023-11-17 11:05:30 +0700

    login

--- Project_Web/pages/login.php
+++ Project_Web/pages/login.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/login_user.css">
+    <link rel="stylesheet" href="././css/login_user.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Ludiflex | Login</title>
     <style>
